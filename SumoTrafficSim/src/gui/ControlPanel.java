868ea7df2d7ad6daadbde8b5be8dcdf78f9ad5d8
AnhNguyen-Olivia <thuyanhnguyen.vietnam@gmail.com>
2025-11-26 22:07:47 +0100

    Slight update

--- SumoTrafficSim/src/gui/ControlPanel.java
+++ SumoTrafficSim/src/gui/ControlPanel.java
@@ -5,52 +5,7 @@ import java.awt.*;
 
 public class ControlPanel extends JPanel {
 
-    public ControlPanel(MapPanel map) {
-        setPreferredSize(new Dimension(220, 650));
-        setBackground(Color.LIGHT_GRAY);
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        add(Box.createVerticalGlue());
-
-        // Simulation Controls header
-        JLabel controlsHeader = new JLabel("Simulation Controls");
-        controlsHeader.setFont(new Font("Arial", Font.BOLD, 16));
-        controlsHeader.setAlignmentX(Component.CENTER_ALIGNMENT);
-        add(controlsHeader);
-        add(Box.createRigidArea(new Dimension(0, 18)));
-
-        // Simulation control buttons - spaced out
-        add(createButton("START", new Color(35, 217, 83), Color.BLACK, e -> map.getEngine().start()));
-        add(Box.createRigidArea(new Dimension(0, 16)));
-        add(createButton("STOP", new Color(226, 70, 60), Color.WHITE, e -> map.getEngine().stop()));
-        add(Box.createRigidArea(new Dimension(0, 16)));
-        add(createButton("ADD VEHICLE", new Color(244, 229, 66), Color.BLACK, e -> map.getEngine().addVehicle()));
-        add(Box.createRigidArea(new Dimension(0, 16)));
-        add(createButton("TOGGLE TRAFFIC LIGHTS", new Color(41, 107, 231), Color.WHITE, e -> map.getEngine().toggleLights()));
-        add(Box.createRigidArea(new Dimension(0, 32)));
-
-        add(Box.createVerticalGlue());
-
-        // Map Editor Tools header
-        JLabel mapToolsHeader = new JLabel("Map Editor Tools");
-        mapToolsHeader.setFont(new Font("Arial", Font.BOLD, 16));
-        mapToolsHeader.setAlignmentX(Component.CENTER_ALIGNMENT);
-        add(mapToolsHeader);
-        add(Box.createRigidArea(new Dimension(0, 18)));
-
-        // Map editor tool buttons
-        add(createButton("ROAD", Color.BLACK, Color.WHITE, e -> map.setTool(MapPanel.Tool.ROAD)));
-        add(Box.createRigidArea(new Dimension(0, 12)));
-        add(createButton("INTERSECTION", Color.BLACK, Color.WHITE, e -> map.setTool(MapPanel.Tool.INTERSECTION)));
-        add(Box.createRigidArea(new Dimension(0, 12)));
-        add(createButton("TRAFFIC LIGHT", Color.BLACK, Color.WHITE, e -> map.setTool(MapPanel.Tool.TL)));
-        add(Box.createRigidArea(new Dimension(0, 12)));
-        add(createButton("SELECT/MOVE", new Color(35, 217, 83), Color.BLACK, e -> map.setTool(MapPanel.Tool.SELECT)));
-        add(Box.createRigidArea(new Dimension(0, 12)));
-        add(createButton("DELETE", new Color(226, 70, 60), Color.WHITE, e -> map.setTool(MapPanel.Tool.DELETE)));
-
-        add(Box.createVerticalGlue());
-    }
-
+    // Create button with center-aligned label
     private JButton createButton(String text, Color bg, Color fg, java.awt.event.ActionListener action) {
         JButton button = new JButton(text);
         button.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -64,5 +19,63 @@ public class ControlPanel extends JPanel {
         button.addActionListener(action);
         button.setBorder(BorderFactory.createLineBorder(bg.darker(), 2));
         return button;
+    }
+
+    public ControlPanel(MapPanel map) {
+        setPreferredSize(new Dimension(220, 650));
+        setBackground(Color.LIGHT_GRAY);
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        // Add vertical glue to push everything to the center
+        add(Box.createVerticalGlue());
+
+        // Simulation Controls header
+        JLabel controlsHeader = new JLabel("Simulation Controls");
+        controlsHeader.setFont(new Font("Arial", Font.BOLD, 16));
+        controlsHeader.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(controlsHeader);
+        add(Box.createRigidArea(new Dimension(0, 18)));
+
+        // Simulation control buttons - spaced out
+        add(createButton("START", new Color(35, 217, 83), Color.BLACK, e -> map.getEngine().start()));
+        add(Box.createRigidArea(new Dimension(0, 16)));
+
+        add(createButton("STOP", new Color(226, 70, 60), Color.WHITE, e -> map.getEngine().stop()));
+        add(Box.createRigidArea(new Dimension(0, 16)));
+
+        add(createButton("ADD VEHICLE", new Color(244, 229, 66), Color.BLACK, e -> map.getEngine().addVehicle()));
+        add(Box.createRigidArea(new Dimension(0, 16)));
+
+        add(createButton("<html><center>TOGGLE<br>TRAFFIC LIGHTS</center></html>", new Color(41, 107, 231), Color.WHITE, e -> map.getEngine().toggleLights()));
+        add(Box.createRigidArea(new Dimension(0, 32)));
+
+        // Add glue between major sections
+        add(Box.createVerticalGlue());
+
+        // Map Editor Tools header
+        JLabel mapToolsHeader = new JLabel("Map Editor Tools");
+        mapToolsHeader.setFont(new Font("Arial", Font.BOLD, 16));
+        mapToolsHeader.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(mapToolsHeader);
+        add(Box.createRigidArea(new Dimension(0, 18)));
+
+        // Map editor tool buttons - spaced out
+        add(createButton("ROAD", Color.BLACK, Color.WHITE, e -> map.setTool(MapPanel.Tool.ROAD)));
+        add(Box.createRigidArea(new Dimension(0, 12)));
+
+        add(createButton("INTERSECTION", Color.BLACK, Color.WHITE, e -> map.setTool(MapPanel.Tool.INTERSECTION)));
+        add(Box.createRigidArea(new Dimension(0, 12)));
+
+        add(createButton("TRAFFIC LIGHT", Color.BLACK, Color.WHITE, e -> map.setTool(MapPanel.Tool.TL)));
+        add(Box.createRigidArea(new Dimension(0, 12)));
+
+        add(createButton("SELECT/MOVE", new Color(35, 217, 83), Color.BLACK, e -> map.setTool(MapPanel.Tool.SELECT)));
+        add(Box.createRigidArea(new Dimension(0, 12)));
+
+        add(createButton("DELETE", new Color(226, 70, 60), Color.WHITE, e -> map.setTool(MapPanel.Tool.DELETE)));
+
+        // Glue at the bottom for final padding
+        add(Box.createVerticalGlue());
     }
 }
